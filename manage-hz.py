ca0ab794caa762b4678faaca6bfbd3ce971d412e
Alexander Jung-Loddenkemper <alexander@julo.ch>
2014-10-11 08:39:31 +0200

    restructure crawler.

--- manage-hz.py
+++ manage-hz.py
@@ -2,9 +2,20 @@
 
 from flask.ext.script import Manager
 from hackzurich import create_app
+from hackzurich import crawler
 
 
 manager = Manager(create_app)
+
+
+@manager.option('-v', '--verbose', help='Print a log')
+def crawl():
+    crawler.crawl()
+
+
+@manager.command
+def wsgi():
+    pass
 
 
 if __name__ == '__main__':
