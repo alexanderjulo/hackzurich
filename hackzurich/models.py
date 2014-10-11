4cd4705a40811bfff5241df834c9a6452e01b1ed
Alexander Jung-Loddenkemper <alexander@julo.ch>
2014-10-11 10:30:15 +0200

    add products & categories (including migrations).

---
+++ hackzurich/models.py
@@ -0,0 +1,24 @@
+from . import db
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    migros_id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    parent_category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+
+    parent_category = db.relationship("Category", uselist=False)
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    name = db.Column(db.String(100))
+    migros_id = db.Column(db.Integer)
+    ean = db.Column(db.String(13))
+    subtitle = db.Column(db.String(100))
+
+    category = db.relationship("Category", uselist=False)
